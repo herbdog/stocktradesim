5eb2099628b02f13befbb1a152b7ba312bc7afcf
Donovan Bai <donovanbai@outlook.com>
2016-07-24 10:36:49 -0700

    ApiTest2 now prompts user input and no longer uses ArrayList

--- ApiTest/ApiTest2.java
+++ ApiTest/ApiTest2.java
@@ -1,40 +1,45 @@
+//go to http://www.jarloo.com/yahoo_finance/ for api spec
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
-import java.util.Timer;
-import java.util.TimerTask;
-
 
 public class ApiTest2 {
-	public static void main(String[] args) throws Exception {
-		//scheduled task timer
-		int delay = 0;
-		int period = 10000; //repeats every 10 seconds
-		Timer timer = new Timer();
-		timer.scheduleAtFixedRate(new TimerTask() {
-			public void run() {
-				try {
-					String strURL = "https://download.finance.yahoo.com/d/quotes.csv?s=goog&f=ab"; //base url using goog
-					ArrayList<String> stockList = new ArrayList<>();
-					stockList.add("goog");
-					URL myURL = new URL(strURL);
-					BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream()));
-					String inputLine;
-					int i = 0;
-					while ((inputLine = in.readLine()) != null) {           
-						System.out.print(stockList.get(i) + ": ");
-						System.out.println(inputLine);
-						i++;
-					}
-					in.close();	
-				} catch (MalformedURLException e) {
-					System.err.println("MalformedURLException thrown");
-				} catch (IOException e) {
-					System.err.println("IOException thrown");
-				}
-			}
-		}, delay, period);
-	}
+    public static void main(String[] args) throws Exception {
+        String strURL = "https://download.finance.yahoo.com/d/quotes.csv?s="; //base URL
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        //prompt user for stocks, append them to URL
+        System.out.println("enter stocks");
+        String stock = stdIn.readLine();
+        while (stock.equals("") == false) { //as long as user enters non-empty line
+            strURL += stock + "+";
+            stock = stdIn.readLine();
+        }
+        strURL = strURL.substring(0, strURL.length()-1); //remove the last "+" from URL
+        strURL += "&f=sl1"; //s = symbol and l1 = last trade price.
+        URL myURL = new URL(strURL);
+	//scheduled task timer
+	int delay = 0;
+	int period = 10000; //repeats every 10 seconds
+	Timer timer = new Timer();
+	timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+		try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream()));
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {           
+                        System.out.println(inputLine);
+                    }
+                    System.out.println();
+                    in.close();	
+                } catch (MalformedURLException e) {
+                    System.err.println("MalformedURLException thrown");
+		} catch (IOException e) {
+                    System.err.println("IOException thrown");
+		}
+            }
+	}, delay, period);
+    }
 }
 
 
